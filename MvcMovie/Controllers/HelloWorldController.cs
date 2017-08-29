20f128d61dc8c59f4eb10ca9dede0e34a6f55c8a
rmaratea <roberto.maratea@quintilesims.com>
2017-08-29 09:35:40 +0200

    first commit of the solution

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            //return View();
+            return "Default action";
+        }
+
+        public string Welcome(string name, short id)
+        {
+            return HttpUtility.HtmlEncode(string.Format("Welcome method {0}, id {1}", name, id));
+        }
+    }
+}
