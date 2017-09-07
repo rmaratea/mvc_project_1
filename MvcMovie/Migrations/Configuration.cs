4760b8f674868f62b61e6c75adeb3c911e4e738f
rmaratea <roberto.maratea@quintilesims.com>
2017-09-07 17:27:17 +0200

    Added model fields validation

--- MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/Migrations/Configuration.cs
@@ -27,7 +27,7 @@ namespace MvcMovie.Migrations
                         ReleaseDate = DateTime.Parse("1989-1-11"),
                         Genre = "Romantic Comedy",
                         Price = 7.99M,
-                        Rating = "3"
+                        Rating = "G"
                     },
 
                      new Movie
@@ -36,7 +36,7 @@ namespace MvcMovie.Migrations
                          ReleaseDate = DateTime.Parse("1984-3-13"),
                          Genre = "Comedy",
                          Price = 8.99M,
-                         Rating = "3"
+                         Rating = "G"
                      },
 
                      new Movie
@@ -45,7 +45,7 @@ namespace MvcMovie.Migrations
                          ReleaseDate = DateTime.Parse("1986-2-23"),
                          Genre = "Comedy",
                          Price = 9.99M,
-                         Rating = "3"
+                         Rating = "G"
                      },
 
                    new Movie
@@ -54,7 +54,7 @@ namespace MvcMovie.Migrations
                        ReleaseDate = DateTime.Parse("1959-4-15"),
                        Genre = "Western",
                        Price = 3.99M,
-                       Rating = "3"
+                       Rating = "G"
                    }
                 );
         }
