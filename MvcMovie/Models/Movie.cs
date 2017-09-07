7698a69d4083caad58b52c189d174daaead5fac1
rmaratea <roberto.maratea@quintilesims.com>
2017-09-07 16:18:23 +0200

    Test on code first db update

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -19,9 +19,10 @@ namespace MvcMovie.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 
-    public class MovieDbContext : DbContext
+    public class MovieDBContext : DbContext
     {
         public DbSet<Movie> Movies { get; set; }
 
